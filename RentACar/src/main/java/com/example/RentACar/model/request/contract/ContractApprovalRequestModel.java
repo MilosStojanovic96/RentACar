9b5902e7cecb6c8ad1c039cd71185071beb76d75
Miloš Stojanović <88903071+MilosStojanovic96@users.noreply.github.com>
2021-10-18 20:20:48 +0200

    Delete RentACar directory

--- RentACar/src/main/java/com/example/RentACar/model/request/contract/ContractApprovalRequestModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.RentACar.model.request.contract;
-
-public class ContractApprovalRequestModel {
-    private boolean approved;
-
-    public boolean isApproved() {
-        return approved;
-    }
-}
